58af5193fc6743d58c4b8824b0e2d5991c5c110f
Miguel Urena <urena_miguel82@outlook.com>
2023-08-03 15:03:50 +0000

    Update Notes

--- src/main/java/playground/App.java
+++ src/main/java/playground/App.java
@@ -1,5 +1,5 @@
 package main.java.playground;
-
+import main.java.HelloWorld;
 import main.java.string.IntroToString;
 
 // Note: The Object is the parent class for ALL objects created in Java.
@@ -9,16 +9,6 @@ public class App {
         
         IntroToString stringsClass = new IntroToString();
         IntroToString stringClass2 = new IntroToString("Day 3 was Great!");
-        
-        System.out.println(stringsClass.helloString);
-        System.out.println(stringClass2.helloString);
-
-        System.out.println(stringsClass.attendance);
-        System.out.println(stringClass2.attendance);
-
-        stringClass2.attendance += 7;
-
-        System.out.println(stringsClass.attendance);
 
         // This variable has a static non access modifier
         System.out.println(IntroToString.attendance);
@@ -31,8 +21,34 @@ public class App {
 
         // Note: denots assignment VS. == denotes comparison
 
+        System.out.println(welcome.equalsIgnoreCase(hello2));
+
+        //.equals allows us to compare strings regardless of their memory locations
+        //if we use == on an impicit String definition , and compare it to a string
+        // that is definied using the 'new' keyword , even if they are the same
+        //we will get a false return. For that reason we have the equals method.
+
+        // .equalsIgnoreCase() - this method allows us to compare strings
+        //regardless of their casing.
+
+        int favNum = HelloWorld.favoriteNumber();
+        System.out.println(favNum);
+        //because the favoriteNumber method has the static non access modifier in its signature
+        //we are able to call the method on the class itself.
+        System.out.println(HelloWorld.favoriteNumber());
+
         System.out.println(welcome == hello); // true
         System.out.println(welcome == hello2); // false
+
+        System.out.println(stringsClass.helloString);
+        System.out.println(stringClass2.helloString);
+
+        System.out.println(stringsClass.attendance);
+        System.out.println(stringClass2.attendance);
+
+        stringClass2.attendance += 7;
+
+        System.out.println(stringsClass.attendance);
 
     }
 
