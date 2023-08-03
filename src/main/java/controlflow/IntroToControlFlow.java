a9dd30fb7912dbaf8608afff0bf7fdc0810b1bc5
Miguel Urena <urena_miguel82@outlook.com>
2023-08-03 15:06:03 +0000

    Day 4 Notes

---
+++ src/main/java/controlflow/IntroToControlFlow.java
@@ -0,0 +1,20 @@
+package main.java.controlflow;
+
+public class IntroToControlFlow {
+    // Control Flow - Control flow statements regulate the flow of the 
+    // control flow (or flow of control) is the order in which individual statements, 
+    // instructions or function calls of an imperative program are executed or evaluated.
+
+    // To practice using the different statement we will create an array. 
+    // Array - structures that are used to store multiple values in a single variable, 
+    // instead of declaring separate variables for each value
+
+
+    // Types of control flow statements
+
+
+
+
+
+    // Practice
+}
